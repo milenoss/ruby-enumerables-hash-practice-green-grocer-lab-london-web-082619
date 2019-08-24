4f36a4d53356acab8a8ec43423864958aaa9273b
Milen Khanal <milenkhanal@gmail.com>
2019-08-24 11:29:33 +0000

    Automatically backed up by Learn

--- practice_grocer.rb
+++ practice_grocer.rb
@@ -4,6 +4,8 @@ def consolidate_cart(cart)
   cart.each do |item_hash| 
   item.each do |item,attribute_hash|
    if  new_cart ||= item
+     new_cart[item][:count] ? new_cart[item][:count] += 1 :
+     new_cart[item][:count] = 1 
      binding.pry
       end
     end
