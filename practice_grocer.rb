4ff9b568bcb6559d440faa9913f6507d51071e21
Milen Khanal <milenkhanal@gmail.com>
2019-08-24 11:25:57 +0000

    Automatically backed up by Learn

--- practice_grocer.rb
+++ practice_grocer.rb
@@ -1,11 +1,12 @@
+require 'pry'
 def consolidate_cart(cart)
   new_cart = {}
   cart.each do |item_hash| 
   item.each do |item,attribute_hash|
    if  new_cart ||= item
      binding.pry
- end
- end
+      end
+    end
  end
  
  
