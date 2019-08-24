519a3e03f7d53d36f0db8e28c3d7947d33bf04e5
Milen Khanal <milenkhanal@gmail.com>
2019-08-24 11:50:44 +0000

    Automatically backed up by Learn

--- practice_grocer.rb
+++ practice_grocer.rb
@@ -12,4 +12,39 @@ def consolidate_cart(cart)
   end
  end
  
-    
+ def apply_coupons(cart,coupons)   
+   coupons.each do |coupon|
+     coupon.each do |attribute, value| 
+       name = coupon[:item]
+       if cart[name] && cart[name][:count] >= coupon[:num]
+         if cart["#{name} W/COUPON"]
+           cart["#{name} W/COUPON"][:count] += 1 
+         elsecart["#{name} W/COUPON "] = {:price => coupon[:cost],
+           :clearance => cart[name][:clearance], :count => 1}
+         end
+         cart[name][:count] -= coupon[:num]
+       end
+     end 
+   end
+   cart
+ end
+ 
+ def apply_clearance(cart)
+   cart.each do |item, attribute_hash|
+     if attribute_hash[:clearance] == true
+       atrribute_hash[:price] = (atrribute_hash[:price] * 0.8.round(2))
+     end
+   end
+   cart
+ end
+ 
+ total = 0 
+ new_cart = consolidate_cart(cart)
+ coupon_cart = apply_coupons(new_cart, coupons)
+ clearance_cart = apply_clearance(coupon_cart)
+ 
+
+ 
+ 
+ 
+   
